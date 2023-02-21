e6dc98c1c1648b21cff3b8917d7dbac0b6abb18e
Andi Faizal <andifaizal647@gmail.com>
2023-02-22 00:15:59 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,159 @@
+import { useState } from 'react';
+import logo from '../src/assets/Frame.svg';
+import doctor from '../src/assets/doctor.png';
+import icons from '../src/assets/icons.png';
+import nurse from '../src/assets/nurse.png';
+import icon1 from '../src/assets/icon1.svg';
+import icon2 from '../src/assets/icon2.svg';
+import icon3 from '../src/assets/icon3.svg';
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div className='absolute w-full py-11 z-10'>
+        <header className='container mx-auto'>
+          <nav className='flex items-center'>
+            <div className='w-2/12 flex items-center'>
+              <img src={logo} alt='holadok' />
+              <h1 className='ml-4 font-sans-pro font-black'>Holadok</h1>
+            </div>
+            <div className='w-7/12'>
+              <ul className='flex items-center gap-x-10'>
+                <li>
+                  <a className='font-sans-pro' href=''>
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a className='font-sans-pro' href=''>
+                    Features
+                  </a>
+                </li>
+                <li>
+                  <a className='font-sans-pro' href=''>
+                    Blog
+                  </a>
+                </li>
+                <li>
+                  <a className='font-sans-pro' href=''>
+                    About Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className='w-3/12 '>
+              <ul className='flex justify-end items-center gap-x-8'>
+                <li>
+                  <a href='' className='font-sans-pro'>
+                    Login
+                  </a>
+                </li>
+                <button className='font-sans-pro border-2 border-black py-2 px-4'>
+                  Book Now
+                </button>
+              </ul>
+            </div>
+          </nav>
+        </header>
+      </div>
+      <div className='container mx-auto relative mb-10'>
+        <span className='bg-[#9BEAE0] absolute -z-10 top-0 right-24 w-[590px] h-[847px] -skew-x-[5deg]'></span>
+        <section className='flex items-center'>
+          <div className='w-5/12'>
+            <div>
+              <h5 className='font-dm-sans text-[#101C45] text-5xl tracking-wide'>
+                Get the healthcare you need, when you need it.
+              </h5>
+            </div>
+            <div className='my-4'>
+              <p className='font-dm-sans text-[#101C45] tracking-wider'>
+                Manage all of your healthcare needs in one convenient place –
+                from booking appointments to tracking your health history.
+              </p>
+            </div>
+            <div>
+              <button className='bg-[#101C45] px-9 py-4 text-white'>
+                {' '}
+                Book An Appointment
+              </button>
+            </div>
+          </div>
+          <div className='w-7/12 flex justify-end relative'>
+            <div className='absolute p-5 left-3 top-60 bg-white z-20 w-[292px] h-[192px] rounded-md drop-shadow-xl'>
+              <div className='flex justify-between items-center'>
+                <div className='flex justify-around items-center gap-x-4'>
+                  <img src={nurse} alt='' />
+                  <h6 className='font-sans-pro text-xs font-semibold'>
+                    Jane Doe
+                  </h6>
+                </div>
+                <div>
+                  <img src={icons} alt='' />
+                </div>
+              </div>
+              <div className='pt-4'>
+                <p className='text-xs text-[#101C45]'>
+                  “The doctor appointment app has been great for me. Scheduling
+                  appointments and getting medication reminders is much easier,
+                  and I love the option for virtual consultations. I'd recommend
+                  this app to others.”
+                </p>
+              </div>
+            </div>
+
+            <div className='pl-24 mt-20 w-[530px] h-[794px]'>
+              <img src={doctor} alt='' />
+            </div>
+          </div>
+        </section>
+        <section className='absolute bottom-0 left-16 right-16 p-10 flex items-center h-[191px] bg-[#203271] text-white shadow-lg'>
+          <div className='flex gap-x-3 items-stretch'>
+            <div>
+              <img src={icon1} alt='' />
+            </div>
+            <div>
+              <h6 className='font-dm-sans font-bold text-lg'>
+                Online appointment scheduling
+              </h6>
+              <p className='font-dm-sans font-normal text-base'>
+                Allows users to schedule appointments with their healthcare
+                provider through the app.
+              </p>
+            </div>
+          </div>
+          <div className='flex gap-x-3 items-stretch'>
+            <div>
+              <img src={icon2} alt='' />
+            </div>
+            <div>
+              <h6 className='font-dm-sans font-bold text-lg'>
+                Medication reminders
+              </h6>
+              <p className='font-dm-sans font-normal text-base'>
+                Allows users to schedule appointments with their healthcare
+                provider through the app.
+              </p>
+            </div>
+          </div>
+          <div className='flex gap-x-3 items-stretch'>
+            <div className=''>
+              <img src={icon3} alt='' />
+            </div>
+            <div>
+              <h6 className='font-dm-sans font-bold text-lg'>
+                Virtual consultations
+              </h6>
+              <p className='font-dm-sans font-normal text-base'>
+                Allows users to schedule appointments with their healthcare
+                provider through the app.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      fontFamily: {
+        'dm-sans': ['DM Sans', 'sans-serif'],
+        'sans-pro': ['Source Sans Pro', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
